0bb30a3c6a3eb82056ec9552ed3667983fc6d7c4
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2025-09-14 17:12:35 +0700

    Init commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@modules': path.resolve(__dirname, 'src/modules'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+});
