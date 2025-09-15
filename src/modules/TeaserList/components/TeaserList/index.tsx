d212ccf1df4a6f352dc4b34969f73864d53ea594
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2025-09-16 01:43:42 +0700

    Add virtuoso library for virtualization list

--- src/modules/TeaserList/components/TeaserList/index.tsx
+++ src/modules/TeaserList/components/TeaserList/index.tsx
@@ -1,13 +1,10 @@
-import { useIntersectionObserver } from '@/hooks/useObserver';
 import { TeaserCard } from '@modules/TeaserList/components/TeaserCard';
 import styles from '@modules/TeaserList/components/TeaserList/TeaserList.module.scss';
 import useTeaserList from '@modules/TeaserList/hooks/useTeaserList';
+import { Virtuoso } from 'react-virtuoso';
 
 export default function TeaserList() {
   const { teasers, isLoading, error, hasMorePages, nextPage } = useTeaserList();
-  const { nodeRef } = useIntersectionObserver({
-    onIntersection: nextPage,
-  });
 
   if (isLoading && !teasers?.length) return <div>Loading...</div>;
   if (error) return <div>Error: {error?.message}</div>;
@@ -16,13 +13,23 @@ export default function TeaserList() {
   return (
     <section className={styles.list}>
       <h2 className={styles.title}>Teaser List</h2>
-      {teasers.map((item) => (
-        <TeaserCard key={item.id} {...item} />
-      ))}
-      {hasMorePages && !isLoading && (
-        <div ref={nodeRef} style={{ height: 1 }} />
-      )}
-      {isLoading && <div>Loading...</div>}
+      <Virtuoso
+        useWindowScroll
+        data={teasers}
+        endReached={hasMorePages ? nextPage : undefined}
+        itemContent={(_, item) => <TeaserCard key={item.id} {...item} />}
+        className={styles.virtuosoContainer}
+        components={{
+          List: ({ children, ...props }) => (
+            <div className={styles.virtuosoList} {...props}>
+              {children}
+            </div>
+          ),
+          Footer: () => {
+            if (isLoading) return <div>Loading...</div>;
+          },
+        }}
+      />
     </section>
   );
 }
