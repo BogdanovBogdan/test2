26eb46bb10ab17464c94322952c5be8a85ad87ee
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2025-09-16 04:05:43 +0700

    Add fallback on error to Image UI component

--- src/UI/Image/index.tsx
+++ src/UI/Image/index.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx';
 import { useState, type SyntheticEvent } from 'react';
+import fallbackImg from './broken-image.png';
 import styles from './Image.module.scss';
 import type { ImageProps } from './types';
 
-const Image = ({ className, onLoad, ...rest }: ImageProps) => {
+const Image = ({ className, onLoad, onError, ...rest }: ImageProps) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const classNameImage = clsx(
@@ -17,7 +18,20 @@ const Image = ({ className, onLoad, ...rest }: ImageProps) => {
     setIsLoading(false);
   };
 
-  return <img className={classNameImage} onLoad={handleLoad} {...rest} />;
+  const handleError = (event: SyntheticEvent<HTMLImageElement, Event>) => {
+    event.currentTarget.src = fallbackImg;
+    if (onError) onError(event);
+    setIsLoading(false);
+  };
+
+  return (
+    <img
+      className={classNameImage}
+      onLoad={handleLoad}
+      onError={handleError}
+      {...rest}
+    />
+  );
 };
 
 export { Image };
